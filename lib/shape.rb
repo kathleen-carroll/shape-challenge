240d256f9f9d92e7b334507ccf12e4b01927ee68
Kathleen Carroll <khakicarroll@comcast.net>
2020-12-04 12:22:28 -0700

    Finish square, rectangle, and triangle class

--- lib/shape.rb
+++ lib/shape.rb
@@ -3,14 +3,41 @@ class Shape
 
   def initialize(name, dimensions)
     @name = name
-    @dimensions = dimensions
+    @dimensions = dimensions #refactor into object or build new class
     @area = calculate_area
   end
 
   def calculate_area
+    # AreaModule.calculate_area(@name)
     if @name == 'circle'
       radius = @dimensions.gsub('radius ', '').to_i
       @area = (Math::PI * (radius ** 2)).round(2) # this could be in abstracted out later
+    elsif @name == 'square' || @name == 'rectangle'
+      length = @dimensions.split("x")[0].to_i
+      width = @dimensions.split("x").last.to_i
+      @area = (length * width).round(2)
+    elsif @name == 'triangle'
+      # a = 0
+      # b = 0
+      # c = 0
+      hash = Hash.new
+      # dimension_array = @dimensions.split(" x ")
+      @dimensions.split(" x ").each {|dimension| hash[dimension[0]] = dimension[-1] }
+
+      # S = 1/2 (A + B + C)
+      # Area = sqrt(s(s-a)(s-b)(s-c))
+      a = hash["a"].to_i
+      b = hash["b"].to_i
+      c = hash["c"].to_i
+
+      sum = a + b + c
+      s = 0.5 * sum
+      
+      @area = Math.sqrt(s * (s - a) * (s - b) * (s - c))
+
+      # require "pry"; binding.pry
+    else
+      "Can't calculate"
     end
   end
 end
--- test/shape_test.rb
+++ test/shape_test.rb
@@ -5,6 +5,9 @@ require './lib/shape'
 class ShapeTest < Minitest::Test
   def setup
     @circle = Shape.new('circle', 'radius 3')
+    @square = Shape.new('square', '4 x 4')
+    @rectangle = Shape.new('rectangle', '4 x 3')
+    @triangle = Shape.new('triangle', 'a3 x b4 x c5')
   end
 
   def test_it_exists
@@ -18,6 +21,24 @@ class ShapeTest < Minitest::Test
 
   def test_it_can_calculate_area
     assert_equal 28.27, @circle.area
+  end
+
+  def test_it_can_be_a_square
+    assert_equal 'square', @square.name
+    assert_equal '4 x 4', @square.dimensions
+    assert_equal 16, @square.area
+  end
+
+  def test_it_can_be_a_rectangle
+    assert_equal 'rectangle', @rectangle.name
+    assert_equal '4 x 3', @rectangle.dimensions
+    assert_equal 12, @rectangle.area
+  end
+
+  def test_it_can_be_a_triangle
+    assert_equal 'triangle', @triangle.name
+    assert_equal 'a3 x b4 x c5', @triangle.dimensions
+    assert_equal 6, @triangle.area
   end
 
 end
