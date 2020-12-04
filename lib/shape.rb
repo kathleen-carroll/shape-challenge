2164776044caa5ca4fade4e33b882dd3edcb1e15
Kathleen Carroll <khakicarroll@comcast.net>
2020-12-04 13:10:40 -0700

    Refactor calculation methods and dimension filtering

--- lib/shape.rb
+++ lib/shape.rb
@@ -45,34 +45,63 @@ class Shape
     # end
   end
 
+  def radius
+    @dimensions.gsub('radius ', '').to_i
+  end
+
+  def length
+    @dimensions.split("x")[0].to_i
+  end
+
+  def width
+    @dimensions.split("x").last.to_i
+  end
+
+  def a
+    a = side_values["a"].to_i
+  end
+
+  def b
+    b = side_values["b"].to_i
+  end
+
+  def c
+    c = side_values["c"].to_i
+  end
+
+  def side_values
+    side_values ||= @dimensions.split(" x ").reduce({}) do |hash, dimension|
+      hash[dimension[0]] = dimension[-1]
+      hash
+    end
+  end
+
   def circle_area
-    radius = dimensions.gsub('radius ', '').to_i
     @area = (Math::PI * (radius ** 2)).round(2)
   end
 
   def square_area
-    length = dimensions.split("x")[0].to_i
-    width = dimensions.split("x").last.to_i
+    # length = dimensions.split("x")[0].to_i
+    # width = dimensions.split("x").last.to_i
     @area = (length * width).round(2)
   end
 
   def rectangle_area
-    length = dimensions.split("x")[0].to_i
-    width = dimensions.split("x").last.to_i
-    @area = (length * width).round(2)
+    square_area
   end
 
   def triangle_area
-    hash = Hash.new
-    # dimension_array = @dimensions.split(" x ")
-    @dimensions.split(" x ").each {|dimension| hash[dimension[0]] = dimension[-1] }
+    # side_values = @dimensions.split(" x ").reduce({}) do |hash, dimension|
+    #   hash[dimension[0]] = dimension[-1]
+    #   hash
+    # end
+
+    # a = side_values["a"].to_i
+    # b = side_values["b"].to_i
+    # c = side_values["c"].to_i
 
     # S = 1/2 (A + B + C)
     # Area = sqrt(s(s-a)(s-b)(s-c))
-    a = hash["a"].to_i
-    b = hash["b"].to_i
-    c = hash["c"].to_i
-
     sum = a + b + c
     s = 0.5 * sum
 
