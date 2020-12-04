ea9c2aa85970f68edd8a6168b6fc0d06eb0c06dc
Kathleen Carroll <khakicarroll@comcast.net>
2020-12-04 12:48:07 -0700

    Refactor calculate area method

--- lib/shape.rb
+++ lib/shape.rb
@@ -8,36 +8,74 @@ class Shape
   end
 
   def calculate_area
+    method("#{@name}_area").call
     # AreaModule.calculate_area(@name)
-    if @name == 'circle'
-      radius = @dimensions.gsub('radius ', '').to_i
-      @area = (Math::PI * (radius ** 2)).round(2) # this could be in abstracted out later
-    elsif @name == 'square' || @name == 'rectangle'
-      length = @dimensions.split("x")[0].to_i
-      width = @dimensions.split("x").last.to_i
-      @area = (length * width).round(2)
-    elsif @name == 'triangle'
-      # a = 0
-      # b = 0
-      # c = 0
-      hash = Hash.new
-      # dimension_array = @dimensions.split(" x ")
-      @dimensions.split(" x ").each {|dimension| hash[dimension[0]] = dimension[-1] }
+    # if @name == 'circle'
+    #   circle_area
+    #   # radius = @dimensions.gsub('radius ', '').to_i
+    #   # @area = (Math::PI * (radius ** 2)).round(2) # this could be in abstracted out later
+    # elsif @name == 'square' || @name == 'rectangle'
+    #   square_area
+    #   # length = @dimensions.split("x")[0].to_i
+    #   # width = @dimensions.split("x").last.to_i
+    #   # @area = (length * width).round(2)
+    # elsif @name == 'triangle'
+    #   triangle_area
+    #   # a = 0
+    #   # b = 0
+    #   # c = 0
+    #   # hash = Hash.new
+    #   # # dimension_array = @dimensions.split(" x ")
+    #   # @dimensions.split(" x ").each {|dimension| hash[dimension[0]] = dimension[-1] }
+    #   #
+    #   # # S = 1/2 (A + B + C)
+    #   # # Area = sqrt(s(s-a)(s-b)(s-c))
+    #   # a = hash["a"].to_i
+    #   # b = hash["b"].to_i
+    #   # c = hash["c"].to_i
+    #   #
+    #   # sum = a + b + c
+    #   # s = 0.5 * sum
+    #   #
+    #   # @area = Math.sqrt(s * (s - a) * (s - b) * (s - c))
+    #
+    #   # require "pry"; binding.pry
+    # else
+    #   "Can't calculate"
+    # end
+  end
 
-      # S = 1/2 (A + B + C)
-      # Area = sqrt(s(s-a)(s-b)(s-c))
-      a = hash["a"].to_i
-      b = hash["b"].to_i
-      c = hash["c"].to_i
+  def circle_area
+    radius = dimensions.gsub('radius ', '').to_i
+    @area = (Math::PI * (radius ** 2)).round(2)
+  end
 
-      sum = a + b + c
-      s = 0.5 * sum
-      
-      @area = Math.sqrt(s * (s - a) * (s - b) * (s - c))
+  def square_area
+    length = dimensions.split("x")[0].to_i
+    width = dimensions.split("x").last.to_i
+    @area = (length * width).round(2)
+  end
 
-      # require "pry"; binding.pry
-    else
-      "Can't calculate"
-    end
+  def rectangle_area
+    length = dimensions.split("x")[0].to_i
+    width = dimensions.split("x").last.to_i
+    @area = (length * width).round(2)
+  end
+
+  def triangle_area
+    hash = Hash.new
+    # dimension_array = @dimensions.split(" x ")
+    @dimensions.split(" x ").each {|dimension| hash[dimension[0]] = dimension[-1] }
+
+    # S = 1/2 (A + B + C)
+    # Area = sqrt(s(s-a)(s-b)(s-c))
+    a = hash["a"].to_i
+    b = hash["b"].to_i
+    c = hash["c"].to_i
+
+    sum = a + b + c
+    s = 0.5 * sum
+
+    @area = Math.sqrt(s * (s - a) * (s - b) * (s - c))
   end
 end
